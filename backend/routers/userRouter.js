b873b32b595d7154eaf1ec01853792562bdd39c6
Giovane Pereira <giovane305pereira@gmail.com>
2022-03-16 18:08:48 -0300

    tela de cadastro implentado

---
+++ backend/routers/userRouter.js
@@ -0,0 +1,101 @@
+import express from "express";
+import expressAsyncHandler from "express-async-handler";
+import User from "../models/userModel";
+import { generateToken, isAuth } from "../utils";
+
+const userRouter = express.Router();
+
+// autenticação do admin
+userRouter.get(
+  "/createadmin",
+  expressAsyncHandler(async (req, res) => {
+    try {
+      const user = new User({
+        name: "admin",
+        email: "admin@example.com",
+        password: "foreverbelle",
+        isAdmin: true,
+      });
+      const createdUser = await user.save();
+      res.send(createdUser);
+    } catch (err) {
+      res.status(500).send({ message: err.message });
+    }
+  })
+);
+
+// autenticação de usuário se já é registrado
+userRouter.post(
+  "/signin",
+  expressAsyncHandler(async (req, res) => {
+    const signinUser = await User.findOne({
+      email: req.body.email,
+      password: req.body.password,
+    });
+
+    if (!signinUser) {
+      res.status(401).send({ message: "Email ou senha inválidos!" });
+    } else {
+      res.send({
+        _id: signinUser._id,
+        name: signinUser.name,
+        email: signinUser.email,
+        isAdmin: signinUser.isAdmin,
+        token: generateToken(signinUser),
+      });
+    }
+  })
+);
+
+// autenticação de usuário ao se registrar
+userRouter.post(
+  "/signup",
+  expressAsyncHandler(async (req, res) => {
+    const user = new User({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+    });
+    const createdUser = await user.save();
+
+    if (!createdUser) {
+      res.status(401).send({ message: "Dados do usuário inválidos!" });
+    } else {
+      res.send({
+        _id: createdUser._id,
+        name: createdUser.name,
+        email: createdUser.email,
+        isAdmin: createdUser.isAdmin,
+        token: generateToken(createdUser),
+      });
+    }
+  })
+);
+
+// atualiza os dados do usuário
+userRouter.put(
+  "/:id",
+  isAuth,
+  expressAsyncHandler(async (req, res) => {
+    const user = await User.findById(req.params.id);
+
+    if (!user) {
+      res.status(404).send({ message: "Usuário não encontrado!" });
+    } else {
+      user.name = req.body.name || user.name;
+      user.email = req.body.email || user.email;
+      user.password = req.body.password || user.password;
+      const updatedUser = await user.save();
+
+      res.send({
+        _id: updatedUser._id,
+        name: updatedUser.name,
+        email: updatedUser.email,
+        isAdmin: updatedUser.isAdmin,
+        token: generateToken(updatedUser),
+      });
+    }
+  })
+);
+
+export default userRouter;
--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,30 @@
 import express from "express";
 import cors from "cors";
 import data from "./data";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import config from "./config.js";
+import userRouter from "./routers/userRouter";
+
+mongoose
+  .connect(config.MONGODB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    console.log("Connected to mongoDB");
+  })
+  .catch((error) => {
+    console.log(error.reason);
+  });
 
 const app = express();
 app.use(cors());
+
+app.use(bodyParser.json());
+
+app.use("/api/users", userRouter);
 
 app.get("/api/products", (req, res) => {
   res.send(data.products);
@@ -17,6 +38,11 @@ app.get("/api/products/:id", (req, res) => {
   } else {
     res.status(404).send({ message: "Produto não encontrado" });
   }
+});
+
+app.use((err, req, res, next) => {
+  const status = err.name && err.name === "ValidationError" ? 400 : 500;
+  res.status(status).send({ message: err.message });
 });
 
 app.listen(5000, () => {
